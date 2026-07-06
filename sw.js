199fb2de31eea827ec7cccba29e5d0d3474d78bd
abhisheksharma489ji <abhisheksharma489ji@gmail.com>
2026-07-06 15:26:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
-self.addEventListener('fetch', function(event) {
-    // This helper script allows the web app to be installable on mobile devices
+const CACHE_NAME = 'raath-academy-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
