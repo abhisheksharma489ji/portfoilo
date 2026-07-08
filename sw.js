e113e512c9e4362def9cf5d249aaf54ec00fee35
abhisheksharma489ji <abhisheksharma489ji@gmail.com>
2026-07-08 16:22:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-const CACHE_NAME = 'raath-academy-cache-v3';
-const ASSETS = [
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
+    caches.open("raath-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html"]);
+    })
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
